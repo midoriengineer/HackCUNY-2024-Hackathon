d4e793a3d88f2188566c955edb6714410d9c5755
NightFall28 <neilh1334@gmail.com>
2024-02-16 17:08:54 -0500

    setting up backend

--- server/server.py
+++ server/server.py
@@ -4,6 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# for testing flask and react connection
+@app.route("/testing")
+def testing():
+    return {"testing1": ["abc", "efg"]}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
