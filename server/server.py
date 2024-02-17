619a09824e78d38043f4d7ed322fa00b11c8e7ee
midoriengineer <115385098+midoriengineer@users.noreply.github.com>
2024-02-17 16:10:46 -0500

    google api stuff

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect, request, session, abort, render_template
+from flask import Flask, jsonify, redirect, request, session, abort, render_template, send_file
 from flask_cors import CORS
 from google_auth_oauthlib.flow import Flow
 from google.oauth2 import id_token
@@ -23,8 +23,6 @@ co = cohere.Client(os.environ.get("COHERE_API_KEY"))
 GOOGLE_CLIENT_ID = os.environ.get("CLIENT_ID")
 client = OpenAI()
 
-
-
 app = Flask(__name__)
 app.secret_key = "XT5PUdwqegbndhgfsbdvH5m79D"
 CORS(app)
@@ -32,6 +30,7 @@ CORS(app)
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 GOOGLE_CLIENT_ID = os.environ.get("CLIENT_ID")
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+token_file_path = 'google_access_token.json'
 
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
@@ -70,12 +69,7 @@ def callback():
     cached_session = cachecontrol.CacheControl(request_session)
     token_request = google.auth.transport.requests.Request(session=cached_session)
 
-    print(credentials.token)
-
-
-    file_path = 'google_access_token.json'
-
-    with open(file_path, 'w') as file:
+    with open(token_file_path, 'w') as file:
         json.dump(
             {"credentials_token": str(credentials.token),
              "credentials_id": str(credentials._id_token)
@@ -95,6 +89,7 @@ def callback():
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
     session.clear()
+    os.remove(token_file_path)
     return redirect("http://localhost:3000")
 
 
@@ -115,6 +110,13 @@ def testing():
     return jsonify(
         {"testing1": "abc", "testing2": "def"}
     )
+@app.route("/google_token", methods=['GET'])
+def google_token():
+    file_path = os.path.join(os.path.dirname(__file__), "google_access_token.json")
+    if os.path.exists(file_path):
+        return send_file(file_path, mimetype='application/json')
+    else:
+        return "File not found", 404
 
 
 @app.route("/embeddings_query", methods=['POST'])
