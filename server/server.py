18564785962b561c05591bca724219685f18ae8b
Junhui Huang <neilh1334@gmail.com>
2024-02-17 11:19:03 -0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -8,6 +8,13 @@ import requests
 import os
 import pathlib
 
+import cohere
+import pandas as pd
+import numpy as np
+import os
+from pinecone import Pinecone, ServerlessSpec
+from openai import OpenAI
+
 app = Flask(__name__)
 app.secret_key = "XT5PUdwqegbndhgfsbdvH5m79D"
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -92,3 +99,35 @@ def testing():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+co = cohere.Client(os.environ.get("COHERE_API_KEY"))
+pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"), environment="venv")
+client = OpenAI()
+
+
+# get data from pinecone
+index_name = 'cohere-pinecone-phish'
+index = pc.Index(index_name)
+
+def embeddings_query(query):
+    q_embed = co.embed(
+        texts=[query],
+        model='embed-english-v3.0',
+        input_type='search_query',
+        truncate='END'
+    ).embeddings
+
+    res = index.query(vector=q_embed, top_k=5, include_metadata=True)
+    results = [{match['metadata']['email']}]
+
+
+def trained_model_query(query):
+    response = co.generate(
+        model='95e83af8-15c7-414a-a743-e5bd36a08b0a-ft',
+        prompt=query
+    )
+    return response.generations[0].text
+
+def general_model_query(query):
+    pass
