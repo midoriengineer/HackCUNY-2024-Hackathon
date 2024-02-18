9455fa02e0d45c5fc8404bd0e07e9db30f347d1d
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2024-02-18 04:43:52 -0500

    some css changes and added email modal to show contents

--- client/src/pages/Dashboard/Inbox.jsx
+++ client/src/pages/Dashboard/Inbox.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React from 'react';
+import EmailModal from './EmailModal';
 
 function Inbox({ folder }) {
+  const [selectedEmail, setSelectedEmail] = React.useState(null);
+
+  const handleEmailClick = (email) => {
+    setSelectedEmail(email);
+  };
+
+  const handleModalClose = () => {
+    setSelectedEmail(null);
+  };
+
   return (
-    <div className="p-4 bg-white rounded-lg shadow">
+    <div className="p-4 bg-white rounded-lg shadow h-screen">
       <h2 className="text-xl font-semibold mb-2">Inbox</h2>
       {folder.length < 1 ? <p>This folder is empty.</p> : 
         folder.map((email, index) => (
-          <div key={index}>
+          <div key={index} className="cursor-pointer" onClick={() => handleEmailClick(email)}>
             <p>
               {email.from} | {email.subject} | {email.phish ? 'Phishing' : 'Safe'}
             </p>
           </div>
         ))
       }
+
+      {selectedEmail && (
+        <EmailModal email={selectedEmail} onClose={handleModalClose} />
+      )}
     </div>
   );
 }
